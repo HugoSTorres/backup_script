08ddfa78661db6cd72d54b48513fe03ff87d35fb
Hugo Torres <hugo.torres1993@gmail.com>
2013-12-30 15:48:24 -0500

    Initial commit

---
+++ backup_script.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Author: Hugo Torres <hugo.torres1993@gmail.com>
+
+# Copyright 2013 Hugo Torres
+
+# This program is free software: you can redistribute it and/or 
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be 
+# useful, but WITHOUT ANY WARRANTY; without even the implied 
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+# PURPOSE.  See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public 
+# License along with this program.  If not, see 
+# <http://www.gnu.org/licenses/>.
+
+backupdir=$HOME/Backup
+backupdate=$(date +%m)-$(date +%d)-$(date +%Y)
+
+shopt -s dotglob
+
+#check the date used for the archive name is correct
+echo "Running backup_script.sh on: $backupdate"
+echo "Is the date correct? 'Y' or 'N'"
+read datecorrect
+if [ $datecorrect = "N" ] || [ $datecorrect = "n" ]; then
+	exit	
+fi
+
+#check if the backup directory exists
+echo "Checking for backup directory"
+if [ ! -d "$backupdir" ]; then
+	echo "Backup directory not found"
+	echo "Creating backup directory $backupdir"
+	mkdir "$backupdir"
+else
+	echo "Backup directory found"
+fi
+
+#generate a list of explicityly installed packages
+echo "Generating list of installed packages"
+pacman -Qe > $backupdir/installed_packages.txt
+echo "List of packages generated"
+
+#copy all files in $HOME to the backup directory
+echo "Copying files into backup directory: $backupdir"
+for file in $HOME/*
+do
+	[[ "$file" = "$backupdir" ]] && continue
+	echo "$file"
+	cp -a "$file" $backupdir
+done
+echo "Done copying files into backup directory"
+
+#create the backup tarball
+echo "Creating backup archive"
+tar -czf $HOME/backup_"$backupdate".tar.gz "$backupdir"
+echo "Backup archive successfully created"
+
+#remove the copied contents
+echo "Removing copied files"
+rm -rf $backupdir
+echo "Done removing copied files"
